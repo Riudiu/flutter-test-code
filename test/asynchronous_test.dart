7496151c8e2b49cb598973324836bab82ccf4c76
Danny-Yun <lacvert13@gmail.com>
2022-09-18 19:49:04 +0900

    부연 설명

--- test/asynchronous_test.dart
+++ test/asynchronous_test.dart
@@ -14,6 +14,9 @@ void main() {
 
   test('completion 함수를 활용한 Future.value 테스트', () {
     var value = Future.value(99);
+
+    /// Future를 테스트 할 때는 기대값을 completion으로 해줘야 한다
+    /// completion 함수는 퓨처가 완료될 때까지 테스트를 종류하지 않도록 해준다
     expect(value, completion(99));
   });
 }

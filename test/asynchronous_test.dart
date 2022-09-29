94014c35d87106a2c0433d6e0575de341059c6a6
Danny-Yun <lacvert13@gmail.com>
2022-09-29 17:55:58 +0900

    발표 준비

--- test/asynchronous_test.dart
+++ test/asynchronous_test.dart
@@ -9,7 +9,7 @@ void main() {
 
   test('Future.value 테스트2', () {
     var value = Future.value(99);
-    expect(value, Future.value(99));
+    expect(value, Future.value(99)); //둘다 퓨처의 인스턴스지만 같은 인스턴스는 아니라서 실패
   });
 
   test('completion 함수를 활용한 Future.value 테스트', () {
